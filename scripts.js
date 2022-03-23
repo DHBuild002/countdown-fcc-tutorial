256e0580bb4ca2883a18dbd67d0c39a2b30dea6a
Dan Haddock <dhbuild.002@gmail.com>
2022-03-23 18:21:59 +0000

    IMP: Add in starting JS and link up script file

--- scripts.js
+++ scripts.js
@@ -21,3 +21,7 @@ const days = [
   "Friday",
   "Saturday",
 ];
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+console.log(items);
