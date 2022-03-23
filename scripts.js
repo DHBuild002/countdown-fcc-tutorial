cea04bdfc411c0ca55d6634b29c76ee4f8f8875d
Dan Haddock <dhbuild.002@gmail.com>
2022-03-23 18:42:14 +0000

    IMP: Add in main day handling for giveaway announcement

--- scripts.js
+++ scripts.js
@@ -24,4 +24,22 @@ const days = [
 const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
-console.log(items);
+let futureDate = new Date(2022, 3, 24, 10, 30, 00);
+// console.log(futureDate)
+
+// Easy Data
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+// Difficult Data
+// The below will use the new Date object to retrieve the chosen month of May
+let month = futureDate.getMonth();
+let day = futureDate.getDay();
+month = months[month];
+day = days[futureDate.getDay()];
+// console.log(months[month]);
+const date = futureDate.getDate();
+
+
+giveaway.textContent = `Giveaway ends on ${day}, ${date} ${month} ${year} - ${hours}:${minutes}am`
